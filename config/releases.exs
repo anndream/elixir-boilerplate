2f94a7e4a1b229cf1dbc6b15bc4edbe9d3d5dd08
Anndream <anndream.com@gmail.com>
2020-11-08 11:35:37 +0700

    Initial commit

---
+++ config/releases.exs
@@ -0,0 +1,87 @@
+import Config
+
+defmodule Environment do
+  @moduledoc """
+  This modules provides various helpers to handle environment metadata
+  """
+
+  def get(key), do: System.get_env(key)
+
+  def get_boolean(key) do
+    case get(key) do
+      "true" -> true
+      "1" -> true
+      _ -> false
+    end
+  end
+
+  def get_integer(key) do
+    case get(key) do
+      value when is_bitstring(value) -> String.to_integer(value)
+      _ -> nil
+    end
+  end
+
+  def get_list_or_first_value(key) do
+    with value when is_bitstring(value) <- get(key),
+         [single_value] <- String.split(value, ",") do
+      single_value
+    else
+      value when is_list(value) -> value
+      _ -> nil
+    end
+  end
+
+  def get_endpoint_static_url_config(nil), do: nil
+  def get_endpoint_static_url_config(""), do: nil
+
+  def get_endpoint_static_url_config(host) do
+    [
+      host: host,
+      scheme: get("STATIC_URL_SCHEME"),
+      port: get("STATIC_URL_PORT")
+    ]
+  end
+end
+
+force_ssl = Environment.get_boolean("FORCE_SSL")
+scheme = if force_ssl, do: "https", else: "http"
+host = Environment.get("CANONICAL_HOST")
+port = Environment.get("PORT")
+
+config :elixir_boilerplate,
+  canonical_host: host,
+  force_ssl: force_ssl
+
+config :elixir_boilerplate, ElixirBoilerplate.Repo,
+  pool_size: Environment.get_integer("DATABASE_POOL_SIZE"),
+  ssl: Environment.get_boolean("DATABASE_SSL"),
+  url: Environment.get("DATABASE_URL")
+
+config :elixir_boilerplate, ElixirBoilerplateWeb.Endpoint,
+  debug_errors: Environment.get_boolean("DEBUG_ERRORS"),
+  http: [port: port],
+  secret_key_base: Environment.get("SECRET_KEY_BASE"),
+  static_url: Environment.get_endpoint_static_url_config(Environment.get("STATIC_URL_HOST")),
+  url: [host: host, scheme: scheme, port: port]
+
+config :elixir_boilerplate, ElixirBoilerplateWeb.Router,
+  session_key: Environment.get("SESSION_KEY"),
+  session_signing_salt: Environment.get("SESSION_SIGNING_SALT")
+
+config :elixir_boilerplate, Corsica, origins: Environment.get_list_or_first_value("CORS_ALLOWED_ORIGINS")
+
+config :elixir_boilerplate,
+  basic_auth: [
+    username: Environment.get("BASIC_AUTH_USERNAME"),
+    password: Environment.get("BASIC_AUTH_PASSWORD")
+  ]
+
+config :sentry,
+  dsn: Environment.get("SENTRY_DSN"),
+  environment_name: Environment.get("SENTRY_ENVIRONMENT_NAME"),
+  included_environments: [Environment.get("SENTRY_ENVIRONMENT_NAME")]
+
+config :new_relic_agent,
+  app_name: System.get_env("NEW_RELIC_APP_NAME"),
+  license_key: System.get_env("NEW_RELIC_LICENSE_KEY")
---
+++ config/test.exs
@@ -0,0 +1,43 @@
+import Config
+
+# Import runtime configuration
+import_config "releases.exs"
+
+defmodule TestEnvironment do
+  @database_name_suffix "_test"
+
+  def get_database_url do
+    url = Environment.get("DATABASE_URL")
+
+    if is_nil(url) || String.ends_with?(url, @database_name_suffix) do
+      url
+    else
+      raise "Expected database URL to end with '#{@database_name_suffix}', got: #{url}"
+    end
+  end
+end
+
+config :elixir_boilerplate, ElixirBoilerplateWeb.Endpoint,
+  http: [port: 4001],
+  server: false,
+  secret_key_base: "G0ieeRljoXGzSDPRrYc2q4ADyNHCwxNOkw7YpPNMa+JgP9iGgJKT4K96Bw/Mf/pd",
+  static_url: [
+    scheme: "https",
+    host: "example.com",
+    port: "443"
+  ],
+  url: [
+    scheme: "https",
+    host: "example.",
+    port: "443"
+  ]
+
+config :elixir_boilerplate, ElixirBoilerplateWeb.Router,
+  session_key: "test",
+  session_signing_salt: "qh+vmMHsOqcjKF3TSSIsghwt2go48m2+IQ+kMTOB3BrSysSr7D4a21uAtt4yp4wn"
+
+config :logger, level: :warn
+
+config :elixir_boilerplate, ElixirBoilerplate.Repo,
+  pool: Ecto.Adapters.SQL.Sandbox,
+  url: TestEnvironment.get_database_url()
